344c9044cbbf2f892603e16659b76995916fdf9e
Tilman Raendchen <tilman@medallyon.me>
2021-11-28 17:59:00 +0000

    Install essential plugins

---
+++ Plugins/AutoSizeComments/Source/AutoSizeComments/Public/AutoSizeCommentsState.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class UEdGraphNode_Comment;
+class SAutoSizeCommentsGraphNode;
+
+struct FASCState
+{
+	FASCState() = default;
+
+	TMap<FGuid, TWeakPtr<SAutoSizeCommentsGraphNode>> CommentToASCMapping;
+
+	void RegisterComment(TSharedPtr<SAutoSizeCommentsGraphNode> ASCComment);
+	void RemoveComment(const UEdGraphNode_Comment* Comment);
+
+	TSharedPtr<SAutoSizeCommentsGraphNode> GetASCComment(const UEdGraphNode_Comment* Comment);
+	bool HasRegisteredComment(UEdGraphNode_Comment* Comment);
+};
